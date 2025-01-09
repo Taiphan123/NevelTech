e07894881a946c647d6d0ab690cb3ecdb9c9dcc7
TaiPV <taiphan0963@gmail.com>
2025-01-09 17:37:54 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/NevelTech/', // Ensure this is correct
   plugins: [react()],
   resolve: {
+    extensions: ['.js', '.jsx'],
     alias: {
       // eslint-disable-next-line no-undef
       '@images': path.resolve(__dirname, 'public/images'),
